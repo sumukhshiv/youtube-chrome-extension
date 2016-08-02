74edffd305f4c7e63eb2d5a03aec732073f0a748
Sumukh Shivakumar <sumukhshiv@yahoo.com>
2016-08-02 16:13:18 -0400

    storing user input

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -1,12 +1,69 @@
-$(document).ready( function() {
-  $('#add-channel-btn').click(function() {
-    var input = $("#search_bar").val();
-    if (input != "") {
-      $('#channel-list').append("<li>" + input + "</li");
-      $("#search_bar").val(""); 
+function get_todos() {
+    var todos = new Array;
+    var todos_str = localStorage.getItem('todo');
+    if (todos_str !== null) {
+        todos = JSON.parse(todos_str);
     }
-  })
-});
+    return todos;
+}
+
+function add() {
+    var task = document.getElementById('task').value;
+
+    var todos = get_todos();
+    todos.push(task);
+    localStorage.setItem('todo', JSON.stringify(todos));
+
+    show();
+
+    return false;
+}
+
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = get_todos();
+    todos.splice(id, 1);
+    localStorage.setItem('todo', JSON.stringify(todos));
+
+    show();
+
+    return false;
+}
+
+function show() {
+    var todos = get_todos();
+
+    var html = '<ul>';
+    for(var i=0; i<todos.length; i++) {
+        html += '<li>' + todos[i] + '<button class="remove" id="' + i  + '">x</button></li>';
+    };
+    html += '</ul>';
+
+    document.getElementById('todos').innerHTML = html;
+
+    var buttons = document.getElementsByClassName('remove');
+    for (var i=0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', remove);
+    };
+}
+
+document.getElementById('add').addEventListener('click', add);
+show();
+
+
+
+//
+// $(document).ready( function() {
+//   $('#add-channel-btn').click(function() {
+//     var input = $("#search_bar").val();
+//
+//     if (input != "") {
+//       $('#channel-list').append("<p>" + input + "</p>");
+//       $("#search_bar").val("");
+//     }
+//   })
+// });
+
 
 
 // Cool GT Analyze swag
@@ -30,4 +87,3 @@ $(document).ready( function() {
   //   });
   // }, false);
 // }, false);
-
