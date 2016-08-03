166e0f4f34b60b0547c52b070264d6b0a215dd5e
Wilbur Shi <wilburshi@sbcglobal.net>
2016-08-02 18:38:30 -0700

    Clean up popup.js for adding channels to local storage

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -1,33 +1,83 @@
-$(document).ready( function() {
-  $('#add-channel-btn').click(function() {
-    var input = $("#search_bar").val();
-    if (input != "") {
-      $('#channel-list').append("<li>" + input + "</li");
-      $("#search_bar").val(""); 
-    }
-  })
-});
+// Background page for debugging purposes (bkg.console.log)
+var bkg = chrome.extension.getBackgroundPage();
 
 
-// Cool GT Analyze swag
-// document.addEventListener('DOMContentLoaded', function() {
-  // var checkPageButton = document.getElementById('checkPage');
-  // checkPageButton.addEventListener('click', function() {
+// Returns all channels from local storage
+function getChannels() {
+  // bkg.console.log("in getChannels");
+  var channels = [];
+  var channels_str = localStorage.getItem('channels');
+  if (channels_str != null && channels_str != undefined) {
+    channels = JSON.parse(channels_str);
+  }
+  return channels;
+}
 
-  //   chrome.tabs.getSelected(null, function(tab) {
-  //     d = document;
+// Event handler for "x" button that removes channel from localStorage 
+// when clicked and updates UI
+function removeChannel() {
+  // bkg.console.log("in removeChannel");
+  var id = $(this).prop('id');
+  var channels = getChannels();
+  channels.splice(id, 1);
+  localStorage.setItem('channels', JSON.stringify(channels));
+  bkg.console.log("we in here removing");
+  showList();
 
-  //     var f = d.createElement('form');
-  //     f.action = 'http://gtmetrix.com/analyze.html?bm';
-  //     f.method = 'post';
-  //     var i = d.createElement('input');
-  //     i.type = 'hidden';
-  //     i.name = 'url';
-  //     i.value = tab.url;
-  //     f.appendChild(i);
-  //     d.body.appendChild(f);
-  //     f.submit();
-  //   });
-  // }, false);
-// }, false);
+  return false;
+}
 
+// Updates UI by adding appropriate html
+function showList() {
+  // bkg.console.log("in showList");
+  var channels = getChannels();
+
+  var html = '<ul>';
+  for (var i=0;i<channels.length;i++) {
+    html += '<li>' + channels[i] + '<button class="remove" id="' + i + '">x</button></li>';
+  }
+  html += '</ul>';
+  $('#channel-list').html(html);
+  $('.remove').click(removeChannel);
+}
+
+// Adds channel to localStorage based on current input in search_bar
+function addChannel() {
+  // bkg.console.log("in addChannel");
+  var input = $('#search_bar').val();
+  var channels = getChannels();
+  channels.push(input);
+  localStorage.setItem('channels', JSON.stringify(channels));
+  showList();
+  return false;
+}
+
+// Start function when document is ready
+function start() {
+  $('#add-channel-btn').click(addChannel);
+  showList();
+}
+
+$(document).ready(start);
+
+// A cool GT Analyze function used for initial testing.
+// function analyzeGTSwag() {
+//   var checkPageButton = document.getElementById('add-channel-btn');
+//   checkPageButton.addEventListener('click', function() {
+
+//     chrome.tabs.getSelected(null, function(tab) {
+//       d = document;
+
+//       var f = d.createElement('form');
+//       f.action = 'http://gtmetrix.com/analyze.html?bm';
+//       f.method = 'post';
+//       var i = d.createElement('input');
+//       i.type = 'hidden';
+//       i.name = 'url';
+//       i.value = tab.url;
+//       f.appendChild(i);
+//       d.body.appendChild(f);
+//       f.submit();
+//     });
+//   }, false);
+// }
